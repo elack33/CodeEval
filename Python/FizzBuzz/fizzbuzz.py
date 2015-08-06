6098699045116c5b59d900a29ea3e01a56468170
Dar Wright <elack33@gmail.com>
2015-08-05 20:40:56 -0700

    Finished FizzBuzz challenge

--- Python/FizzBuzz/fizzbuzz.py
+++ Python/FizzBuzz/fizzbuzz.py
@@ -1,18 +1,30 @@
-__author__ = 'Dar'
+import sys
+import os
 
 
 class FizzBuzz(object):
 
     def __init__(self):
-        # self.validate_input(3, 5, 50)
-        self.import_list('Fizzbuzz_input.txt')
+        self.line_count = 0
+        self.filename = ''
+        self.check_file()
+
+    def check_file(self):
+        self.filename = str(sys.argv[1])
+
+        if os.path.exists(self.filename):
+            self.import_list(self.filename)
+        else:
+            print "File not found, please try again."
 
     def import_list(self, filename):
         with open(filename) as load_import_data:
             for line in load_import_data:
-                x = line.split(' ')
-                y = line.split(' ')
-                n = line.split(' ')
+                line = line.split()
+                n = int(line.pop())
+                y = int(line.pop())
+                x = int(line.pop())
+                self.line_count += 1
                 self.validate_input(x, y, n)
 
     def validate_input(self, x, y, n):
@@ -21,22 +33,27 @@ class FizzBuzz(object):
                 if 20 < n < 101:
                     self.fizzb(x, y, n)
                 else:
-                    print "Invalid number range found.\n"
+                    print "N value is out of range, skipping this line: {}.\n".format(self.line_count)
+                    return
             else:
-                print "Y value is out of range.\n"
+                print "Y value is out of range, skipping this line: {}.\n".format(self.line_count)
+                return
         else:
-            print "X value is out of range.\n"
+            print "X value is out of range, skipping this line: {}.\n".format(self.line_count)
+            return
 
     def fizzb(self, x, y, n):
-        n = int(n)
-        for num in range(1, n):
+        for num in range(1, n +1):
             if (num % x == 0) & (num % y == 0):
                 print 'FB',
+
             else:
                 if num % x == 0:
                     print 'F',
                 if num % y == 0:
                     print 'B',
+                if num == n:
+                    print '\n'
                 else:
                     print num,
 
